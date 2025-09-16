c9e0c853e96d35b4c33d116670425139acf6059b
JirzyKerklaan <jirzykerklaan@gmail.com>
2025-09-16 15:32:26 +0200

    restructured server.ts and extracted into app.ts

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import expressLayouts from 'express-ejs-layouts';
+
+import { sessionMiddleware } from '../core/middlewares/session';
+import { globalLocals } from '../core/middlewares/globalLocales';
+import router from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Views
+app.set('view engine', 'ejs');
+app.set('views', [
+    path.join(process.cwd(), 'src/templates/'),
+    path.join(process.cwd(), 'src/blocks/')
+]);
+
+// Layouts
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+
+// Middleware
+app.use(sessionMiddleware);
+app.use(globalLocals);
+
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+// Routes
+app.use(router);
+
+export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,14 @@
-export { default as managerRoutes } from './manager';
-export { default as routes } from './routes';
-export { default as search } from './search';
-export { default as upload } from './upload';
+import { Router } from 'express';
+import routes from './routes';
+import upload from './upload';
+import search from './search';
+import managerRoutes from './manager';
+
+const router = Router();
+
+router.use('/', routes);
+router.use('/', upload);
+router.use('/', search);
+router.use('/manager', managerRoutes);
+
+export default router;
