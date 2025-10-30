b01cd5ca12f8bb80c9d4b88bdc345e7c71203865
JirzyKerklaan <jirzykerklaan@gmail.com>
2025-10-30 16:37:53 +0100

    added CRUD for entries

--- core/manager/controllers/index.ts
+++ core/manager/controllers/index.ts
@@ -1,2 +1,3 @@
 export { default as collectionController } from './collectionController';
 export { default as blockController } from './blockController';
+export { default as entryController } from './entryController';
