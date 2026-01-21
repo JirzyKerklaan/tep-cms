64ca37dbbdde89fe0cf7e7426bb4ef3f3715e6ab
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-01-21 14:20:36 +0100

    :art: Moved isAuthenticated to seperate file

--- core/middlewares/isAuthenticated.ts
+++ core/middlewares/isAuthenticated.ts
@@ -1,6 +1,13 @@
-function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+import { Request, Response, NextFunction } from "express";
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
   if (req.session && req.session.user) {
     return next();
   }
-  return res.redirect('/login');
+
+  return res.redirect("/manager/login");
 }
