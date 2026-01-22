06a8e41b5144984090e086ee66e59979cefe1c50
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-01-22 15:26:42 +0100

    :sparkles: Added redirect handler

--- core/interfaces/Redirect.ts
+++ core/interfaces/Redirect.ts
@@ -1,7 +1,7 @@
 import {BaseEntity} from "./BaseEntity";
 
 export interface Redirect extends BaseEntity {
-    oldSlug: string;
-    newSlug: string;
-    type: number; // 301 (permanent) OR 302 (temporary)
+    from: string;
+    to: string;
+    permanent?: boolean;
 }
--- core/middlewares/handleRedirects.ts
+++ core/middlewares/handleRedirects.ts
@@ -1,21 +1,18 @@
 // middleware/handleRedirects.ts
 import { Request, Response, NextFunction } from 'express';
 import { Redirect } from "../interfaces/Redirect";
+import {redirectService} from "../services/redirectService";
 
-const redirects: Redirect[] = [
-    // read from file
-];
+export async function handleRedirects(req: Request, res: Response, next: NextFunction) {
+    const redirects: Redirect[] =
+        await redirectService.getById<Redirect[]>('globals', 'redirects');
 
-/**
- * Middleware to handle URL redirects
- */
-export function handleRedirects(req: Request, res: Response, next: NextFunction) {
-    const path = req.path.replace(/\/$/, "");
-
-    const redirect = redirects.find(r => r.oldSlug === path);
+    const normalize = (p: string) => p.replace(/\/$/, '');
+    const path = normalize(req.originalUrl.split('?')[0]);
+    const redirect = redirects.find(r => normalize(r.from) === path);
 
     if (redirect) {
-        return res.redirect(redirect.type, redirect.newSlug);
+        return res.redirect(redirect.permanent ? 301 : 302, redirect.to);
     }
 
     next();
